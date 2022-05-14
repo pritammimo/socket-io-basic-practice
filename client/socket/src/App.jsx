1a0c2c6b97d940e0929d38f7e18cd57205223731
pritamcn <pritams@capitalnumbers.com>
2022-05-14 22:12:59 +0530

    push

--- client/socket/src/App.jsx
+++ client/socket/src/App.jsx
@@ -4,8 +4,14 @@ const socket=io("http://localhost:3001");
 const App = () => {
   const [message,setMesage]=useState("");
   const [messageRecieved, setMessageReceived] = useState("");
+  const [joinRoom,setjoinRoom]=useState("");
   const sendMessage=()=>{
-    socket.emit("send_message",{message})
+    socket.emit("send_message",{message,joinRoom})
+  }
+  const joinRoomHandler=()=>{
+    if(joinRoom!==""){
+      socket.emit("join_room",joinRoom)
+    }
   }
   useEffect(() => {
     socket.on("receive_message",(data)=>{
@@ -14,6 +20,14 @@ const App = () => {
   }, [socket]);
   return (
     <div className='App'>
+       <input 
+     value={joinRoom}
+     onChange={(event)=>{
+       setjoinRoom(event.target.value)
+     }}
+     />
+      <button onClick={joinRoomHandler}>Join Room</button>
+
      <input 
      value={message}
      onChange={(event)=>{
--- server/index.js
+++ server/index.js
@@ -13,9 +13,15 @@ const io=new Server(server,{
 })
 io.on("connection",(socket)=>{
    console.log(`User Connected:${socket.id}`);
-   socket.on("send_message",(data)=>{
-       socket.broadcast.emit("receive_message",data)
-   })
+//    socket.on("send_message",(data)=>{
+//        socket.broadcast.emit("receive_message",data)
+//    })
+socket.on("join_room",(data)=>{
+    socket.join(data);
+});
+socket.on("send_message",(data)=>{
+    socket.to(data.joinRoom).emit("receive_message",data);
+})
 })
 server.listen(3001,()=>{
     console.log("server is running")
